a15397c66fb229573ced8a9a1b328b098b0723ca
Kamil Baczynski <kamil-baczynski1@wp.pl>
2019-08-25 12:39:42 +0200

    Creating JS buttons functions

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,11 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
+    }, function (err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
@@ -16,7 +16,28 @@ if ('serviceWorker' in navigator) {
 
 // place your code below
 
+const buttonPlus = document.querySelector('.button__plus--js');
+const buttonMinus = document.querySelector('.button__minus--js');
+const glassValue = document.querySelector('.glass__counter--js')
+const key = new Date().toISOString().slice(0, 10);
 
-console.log(`Hello world!`);
+if (localStorage.getItem(key)) {
+  glassValue.innerHTML = localStorage.getItem(key);
+} else {
+  glassValue.innerHTML = '0';
+}
 
+buttonPlus.addEventListener('click', (e) => {
+  localStorage.setItem(key, parseInt(glassValue.innerHTML, 10) + 1);
+  glassValue.innerHTML = localStorage.getItem(key);
+})
+
+buttonMinus.addEventListener('click', (e) => {
+  if (localStorage.getItem(key) > 0) {
+    localStorage.setItem(key, parseInt(glassValue.innerHTML, 10) - 1);
+    glassValue.innerHTML = localStorage.getItem(key);
+  } else {
+    return;
+  }
+})
 
